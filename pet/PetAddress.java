ceb03bd2b102e6ca7743b2e47a95dee7399ed407
andredeomondes <andredeomondes.dev@gmail.com>
2025-11-11 13:48:26 -0300

    Creating PetAddres.java

---
+++ pet/PetAddress.java
@@ -0,0 +1,52 @@
+package pet;
+
+import java.util.Objects;
+
+public class PetAddress {
+    private String street;
+    private Integer number;
+    private String city;
+
+    public PetAddress(String street, Integer number, String city) {
+        this.street = street;
+        this.number = number;
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PetAddress petAdress = (PetAddress) o;
+        return Objects.equals(street, petAdress.street) && Objects.equals(number, petAdress.number) && Objects.equals(city, petAdress.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(street, number, city);
+    }
+
+}
